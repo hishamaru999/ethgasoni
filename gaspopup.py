61cc06e730904fce60b722accb08201a4790bc94
hishamaru999 <58763968+hishamaru999@users.noreply.github.com>
2021-01-14 13:38:46 -0800

    added BoxLayout

--- gaspopup.py
+++ gaspopup.py
@@ -1,3 +1,8 @@
+import requests
+import time
+import datetime
+from playsound import playsound
+import defipulse_credentials
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
@@ -17,3 +22,30 @@ class GasApp(App):
 
 if __name__ == "__main__":
     GasApp().run()
+
+
+# Get ETH Gas Station data from defipulse API
+while (True):
+    # Get Ethereum price in USD from CoinGecko
+    request_price = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
+    price = request_price.json()['ethereum']['usd']
+
+    # Get gas data from EthGasStation
+    request_data = requests.get(f'https://ethgasstation.info/api/ethgasAPI.json?api-key={defipulse_credentials.defipulseApikey}')
+    data = request_data.json()
+
+
+    # In Gwei
+    fast_gwei = data['fast'] / 10
+    average_gwei = data['average'] / 10
+    slow_gwei = data['safeLow'] / 10
+    trader_gwei = data['fastest'] / 10
+
+
+# Format time
+now = date.today()
+format = "%m/%d/%Y %H:%M:%S"
+thetime = now.strftime(format)
+
+
+print(f'Time {thetime}:  Trader: {fast_gwei}')
