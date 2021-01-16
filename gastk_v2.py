38b57447cab2abc7b3fccfe6da96c05fec01e23d
hishamaru999 <58763968+hishamaru999@users.noreply.github.com>
2021-01-15 20:31:13 -0800

    cleaned up code to new format

--- gastk_v2.py
+++ gastk_v2.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python
 # ETH Gas Station via DeFi Pulse API - using Tkinter
-import requests
+try:
+    import tkinter as tk
+except ImportError:
+    import Tkinter as tk
 from tkinter import Label
 from tkinter import Tk
 from PIL import ImageTk, Image
 import time
+import requests
 import datetime
 import defipulse_credentials
 
 root = Tk()
 root.title('ETH Gas Station')
-root.geometry("400x400")
+root.geometry("200x200")
 thebackground = "white"
 root.config(background=thebackground)
 
-img = Image.open("f:/egs_transparent.png")
+img = Image.open("f:/ethgas.ico")
 img = ImageTk.PhotoImage(img)
 
 
@@ -26,44 +30,38 @@ averageGasLabel = Label(root, font=("Calibri bold", 20), bg="white")
 averageGasLabel.grid(row=2, sticky="N", padx=100)
 slowgasLabel = Label(root, font=("Calibri bold", 20), bg="white")
 slowgasLabel.grid(row=3, sticky="N", padx=100)
-timeGasLabel = Label(root, font=("Calibri bold", 12), bg="white")
+timeGasLabel = Label(root, font=("Calibri bold", 10), bg="white")
 timeGasLabel.grid(row=4, sticky="N", padx=80)
 
 
 # Get gas data from EthGasStation API
 def getGas():
-    request_data = requests.get(f'https://ethgasstation.info/api/ethgasAPI.json?api-key={defipulse_credentials.defipulseApikey}')
-    data = request_data.json()
-    # In Gwei
-    fast_gwei = data['fast'] / 10
-    average_gwei = data['average'] / 10
-    slow_gwei = data['safeLow'] / 10
-    trader_gwei = data['fastest'] / 10
+        request_data = requests.get(f'https://ethgasstation.info/api/ethgasAPI.json?api-key={defipulse_credentials.defipulseApikey}')
+        data = request_data.json()
+        # In Gwei
+        fast_gwei = data['fast'] / 10
+        average_gwei = data['average'] / 10
+        slow_gwei = data['safeLow'] / 10
+        trader_gwei = data['fastest'] / 10
 
-    traderGasLabel.config(text=trader_gwei)
-    fastGasLabel.config(text=fast_gwei)
-    averageGasLabel.config(text=average_gwei)
-    slowgasLabel.config(text=slow_gwei)
+        traderGasLabel.config(text=trader_gwei)
+        fastGasLabel.config(text=fast_gwei)
+        averageGasLabel.config(text=average_gwei)
+        slowgasLabel.config(text=slow_gwei)
 
-    # Timestamp
-    date = datetime.datetime.today()
-    now = date.today()
-    format = "%a, %b %d %H:%M:%S"
-    timestamped = now.strftime(format)
+        # Timestamp
+        date = datetime.datetime.today()
+        now = date.today()
+        format = "%a, %b %d %H:%M:%S"
+        timestamped = now.strftime(format)
 
-    timeGasLabel.config(text=timestamped)
-   
-    
-    # Update gas data from API
-    #traderGasLabel.after(60000, getGas())
-    #fastGasLabel.after(60000, getGas())
-    #averageGasLabel.after(60000, getGas())
-    #slowgasLabel.after(60000, getGas())
-
-    root.update()
+        timeGasLabel.config(text=timestamped)
 
 
+        root.update()
 
+
+# Call the function to get the API data
 getGas()
 
 root.mainloop()
